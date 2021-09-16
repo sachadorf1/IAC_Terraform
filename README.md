155cf79e4bcf0e2b7d9c2542ce7ca049f48639a8
Sacha <sdorf@spartaglobal.com>
2021-09-16 16:12:36 +0100

    Updated README and added terraform files

--- README.md
+++ README.md
@@ -5,9 +5,11 @@
 ##### Securing AWS keys for Terraform
 
 
-### What is Terraform
+### What is Terraform?
 - Open-source infrastructure as code software tool that provides a consistent Command Line Interface (CLI) workflow to manage hundreds of cloud services.
 
+### `terraform init`
+- 
 
 ### `terraform plan`
 - Creates an execution plan
@@ -19,9 +21,11 @@
 - Destroys all remote objects managed by a particular Terraform configuration
 - terraform apply -destroy
 
-### Tasks
-
+### Setting up Terraform
 - Create env var to secure AWS keys
+    - In Windows, `Edit the system environment variables` -> `Advanced` -> `Environment Variables...` -> Under `User variables for Sacha`, click `New...`
+    - Variable Name: AWS_ACCESS_KEY_ID -> Variable Value: (copy value from excel file)
+    - Variable Name: AWS_SECRET_ACCESS_KEY -> Variable Value: (copy value from excel file)
 - Restart the terminal
 - Create a file called main.tf
 - add the code to initialise terrafrom with provider AWS
@@ -57,3 +61,91 @@ resource "aws_instance" "app_instance" {
 
 - `terraform plan`
 - `terraform apply` -> `yes`
+
+- Instance should be running
+
+- `terraform destroy` to delete instance you have created
+
+### Create a VPC
+
+- delete your customised VPC and resources created inside the VPC
+- then create a VPC with Terraform
+
+- See [Networking documentation](https://github.com/sachadorf1/SRE_AWS_VPC_Networking) repo for setting up a VPC on AWS. We are using the same method but using Terraform.
+
+### step 1 create a vpc with your CDIR block
+- Create variable.tf folder and enter the following (using your own CIDR block e.g. 10.106.0.0/16):
+```
+resource "aws_vpc" "main" {
+  cidr_block       = "10.106.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}  
+```
+- Run `terraform plan`
+- Run `terraform apply`
+
+- get the VPC ID from aws or terraform logs
+
+### step 2 create a public subnet using VPC ID
+- Add this to your main.tf file
+```
+resource "aws_subnet" "sre_sacha_public" {
+  vpc_id     = "vpc-0ca8e735b4084d9cc"
+  cidr_block = "10.106.0.0/24"
+
+  tags = {
+    Name = "sre_sacha_public"
+  }
+}
+```
+
+### Step 3 create 
+
+
+
+## Data
+
+Now we're building quickly. Let's add a few more parts of our architecture. For example, our subnet is currently using the default route table which is private. Let's create a new route table and associate it with our public subnet.
+
+
+
+> EXERCISE ( 5 Minutes ) : Find the resource needed to create a route table.
+
+```
+resource "aws_route_table" "public" {
+vpc_id = "${var.vpc_id}" route {
+cidr_block = "0.0.0.0/0"
+gateway_id = "????"
+} tags = {
+Name = "${var.name}-public"
+}
+}
+```
+
+- grab a reference to the internet gateway for our VPC
+data
+ "aws_internet_gateway" "default" {
+filter {
+name = "attachment.vpc-id"
+values = ["${var.vpc_id}"]
+}
+}
+
+
+variable "aws_key_name" {
+
+    default = "sre_key"
+
+}
+
+
+
+variable "aws_key_path" {
+
+    default = "~/.ssh/sre_key.pem"
+
+}
--- main.tf
+++ main.tf
@@ -6,17 +6,33 @@ provider "aws" {
 }
 
 
+# resource "aws_instance" "app_instance" {
+#   ami = "ami-00e8ddf087865b27f"
+#   instance_type = "t2.micro"
+#   associate_public_ip_address = true
+#   tags = {
+#       Name = "sre_sacha_terraform_app"
+#   }
+#   key_name = "sre_key"
+# }
+
+
+
+
 # Let's start with Launching the EC2 instance using the app AMI
 # define the resource name
 
-resource "aws_instance" "app_instance" {
-  ami = "ami-00e8ddf087865b27f"
-  instance_type = "t2.micro"
-  associate_public_ip_address = true
-  tags = {
-      Name = "sre_sacha_terraform_app"
-  }
-}
+# resource "aws_instance" "app_instance" {
+#   ami = "ami-00e8ddf087865b27f"
+#   vpc_id=
+#   subnet=
+#   sg=
+#   instance_type = "t2.micro"
+#   associate_public_ip_address = true
+#   tags = {
+#       Name = "sre_sacha_terraform_app"
+#   }
+# }
 
 # ami id ` `
 
@@ -24,3 +40,114 @@ resource "aws_instance" "app_instance" {
 # AWS keys set is already done (changed in environment variables)
 # public ip
 # type of the instance `t2micro` 
+
+
+# step 1 create a vpc with your CDIR block
+# run terraform plan then terraform apply
+# get the VPC ID from aws or terraform logs
+
+resource "aws_vpc" "sre_sacha_vpc" {
+  cidr_block       = "10.106.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "sre_sacha_vpc"
+  }
+}  
+
+resource "aws_subnet" "sre_sacha_public" {
+  vpc_id     = "vpc-0ca8e735b4084d9cc"
+  cidr_block = "10.106.0.0/24"
+  map_public_ip_on_launch = "true"  # Makes this a public subnet
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "sre_sacha_public"
+  }
+}
+
+# resource "aws_route_table" "public" {
+# vpc_id = "${var.vpc_id}"
+
+
+
+# route {
+# cidr_block = "0.0.0.0/0"
+# gateway_id = "${data.aws_internet_gateway.default.id}"
+# }
+
+
+
+# tags = {
+# Name = "${var.name}-public"
+# }
+# }
+
+
+resource "aws_security_group" "sr_sacha_app_group"  {
+  name = "sre_sacha_app_sg_terraform"
+  description = "sre_sacha_app_sg_terraform"
+  vpc_id = "vpc-0ca8e735b4084d9cc" # attaching the SG with your own VPC
+  ingress {
+    from_port       = "80"
+    to_port         = "80"
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]   
+  }
+  ingress {
+    from_port       = "22"
+    to_port         = "22"
+    protocol        = "tcp"
+    cidr_blocks     = ["86.155.183.106/32"]  
+  }
+    ingress {
+    from_port       = "3000"
+    to_port         = "3000"
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]  
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1" # allow all
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sre_sacha_app_sg_terraform"
+  }
+}
+
+resource "aws_internet_gateway" "sre_sacha_terraform_ig" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "sre_sacha_terraform_ig"
+  }
+}
+
+resource "aws_route_table" "sre_sacha_rt-public" {
+vpc_id = "${var.vpc_id}"
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = "igw-0c908535cb474f16a"
+}
+tags = {
+Name = "sre_sacha_rt-public"
+}
+}
+
+resource "aws_route" "sre_sacha_route_ig_connection" {
+    route_table_id = var.def_route_table_id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = var.internet_gateway_id
+}
+
+
+
+
+# resource "aws_internet_gateway" "sre_sacha_igw" {
+#   vpc_id = "vpc-0ca8e735b4084d9cc"
+#   tags = {
+#     Name = "sre_sacha_igw"
+#   }
+# }
